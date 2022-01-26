1fa6ef21f34a2a95b946334fca8da7a3f649d684
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-01-26 11:44:48 -0500

    finished pair programming

--- easy2.js
+++ easy2.js
@@ -1,27 +1,3 @@
-/*const prime = [];
-const input =[27, 49, 113, 96, 51, 79, 118, 007] //[112, 4, 62, 138, 90, 10, 12, 13]
-
-console.log("New Attempt")
-
-
-
-function isItPrime (x) {
-    for(i=0; i<x.length; i++)
-    {
-        if (x[i]%2===0)
-        {
-            x.splice(i, 1);
-            i=-1;
-            console.log(x);
-        }
-
-
-    }
-
-        console.log(x);
-    }
-
-isItPrime(input);*/
 const input = [10, 18, 19, 29, 33, 35, 47, 66, 83] 
 const prime = [];
 
